4f15e6939f2456f5e2f37d8e80a58d8f6966a48d
afifa-azim <azimafifa86@gmail.com>
2025-02-09 21:29:28 +0600

    added while loop

--- loop.js
+++
@@ -1,9 +0,0 @@
-//for loop
-const fruits = ['apple', 'banana', 'orange', 'guava'];
-// for(){
-
-// }
-for(const fruit of fruits){
-    console.log(fruit);
-    console.log(I want to eat);
-}
---
+++ while-loop.js
@@ -0,0 +1,37 @@
+//for loop
+const fruits = ['apple', 'banana', 'orange', 'guava'];
+// for(){
+
+// }
+for (const fruit of fruits){
+    console.log(fruit);
+    console.log('I want to eat');
+}
+
+//while loop
+// let num=1;
+//  while(num < 10){
+//      console.log(num);
+//      num++;
+//  }
+
+// adding sum
+// let num = 1;
+// let sum = 0;
+// while (num <= 10){
+//     console.log(num);
+//     sum = sum + num;
+//     console.log('sum:', sum);
+//     num++;
+// }
+
+// even number
+let num = 1;
+while (num <= 10){
+    console.log(num);
+
+    if(num % 2 === 0){
+         console.log('even number:',num)
+         }
+         num++;
+}
